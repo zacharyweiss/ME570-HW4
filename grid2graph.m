95f8268b3523e002e879e4cdd113296307008835
Zachary Weiss <zachary.weiss.acct@gmail.com>
2020-11-11 00:00:27 -0500

    Minor bugs

--- grid2graph.m
+++ grid2graph.m
@@ -2,6 +2,8 @@
 %The function should return a  graphVector structure described by the inputs. See
 %Figure  fig:grid2graph for an example of the expected inputs and outputs.
 function [graphVector]=grid2graph(grid)
+%make sure values in F are logicals
+grid.F=logical(grid.F);
 
 %total number of nodes in the graph
 NNodes=sum(grid.F(:));
@@ -13,16 +15,19 @@ szGrid=size(grid.F);
 
 %this array keeps, for each non-false element of grid.F, the
 %corresponding index in graphVector
-idxMap=zeros(size(grid.F));
-idxMap(grid.F)=1:NNodes;
+%Note that to get the right ordering w.r.t. the definition of grid given in
+%HW2, we need use transposes
+idxMap=zeros(fliplr(size(grid.F)));
+idxMap(grid.F')=1:NNodes;
+idxMap=idxMap';
 
 %fill in the x and neighbors fields
-for iXCoord=1:szGrid(2)
-    for iYCoord=1:szGrid(1)
-        if grid.F(iYCoord,iXCoord)
-            idxGraph=idxMap(iYCoord,iXCoord);
+for iXCoord=1:szGrid(1)
+    for iYCoord=1:szGrid(2)
+        if grid.F(iXCoord,iYCoord)
+            idxGraph=idxMap(iXCoord,iYCoord);
             graphVector(idxGraph).x=[grid.xx(iXCoord);grid.yy(iYCoord)];
-            idxNeighborsF=grid2graph_computeNeighbors(iYCoord,iXCoord,grid.F);
+            idxNeighborsF=grid2graph_computeNeighbors(iXCoord,iYCoord,grid.F);
             graphVector(idxGraph).neighbors=idxMap(sub2ind(szGrid,idxNeighborsF(1,:),idxNeighborsF(2,:)))';
         end
     end
--- grid2graph_test.m
+++ grid2graph_test.m
@@ -1,4 +1,11 @@
 function grid2graph_test
-s=load('graph_testData');
-graphVectorMedium=grid2graph(s.gridMedium);
-graph_plot(graphVectorMedium)
+%Example from Fig.1 in the assignment
+grid.xx=[1;2];
+grid.yy=[1;2;3];
+grid.F=false(2,3);
+grid.F(1,:)=true;
+grid.F(2,1)=true;
+disp('grid.F')
+disp(grid.F)
+graphVectorMedium=grid2graph(grid);
+graph_plot(graphVectorMedium,'nodelabels',true)
