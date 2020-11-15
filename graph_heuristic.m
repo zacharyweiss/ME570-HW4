3ad616ea9e63ccc547b0e040e4a8f9cc9a444764
Zachary Weiss <zachary.weiss.acct@gmail.com>
2020-11-15 00:54:59 -0500

    Update graph_heuristic.m

--- graph_heuristic.m
+++ graph_heuristic.m
@@ -2,5 +2,6 @@
 %Computes the heuristic  @x   h given by the Euclidean distance between the nodes
 %with indexes  @x   idxX and  @x   idxGoal.
 function [hVal]=graph_heuristic(graphVector,idxX,idxGoal)
-    hVal = norm([graphVector(idxX).x graphVector(idxGoal).x]);
+    %hVal = norm([graphVector(idxX).x graphVector(idxGoal).x]);
+    hVal = euclideanDistMatrix(graphVector(idxX).x,graphVector(idxGoal).x);
 end
