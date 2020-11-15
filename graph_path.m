454e619d42742257e565278373983558b6d8fd70
Zachary Weiss <zachary.weiss.acct@gmail.com>
2020-11-15 15:17:40 -0500

    Backpointers

--- graph_path.m
+++ graph_path.m
@@ -3,3 +3,10 @@
 % @x   graphVector to the one with index  @x   idxStart node, and returns the 
 %coordinates (not indexes) of the sequence of traversed elements.
 function [xPath]=graph_path(graphVector,idxStart,idxGoal)
+    idx = idxGoal;
+    xPath = graphVector(idxGoal).x;
+    while idx ~= idxStart
+        idx = graphVector(idx).backpointer;
+        xPath = [graphVector(idx).x xPath];
+    end
+end
