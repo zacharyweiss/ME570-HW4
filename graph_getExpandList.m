4b8bf467cd1f1dbeb92e29cd7081973f857228f4
Zachary Weiss <zachary.weiss.acct@gmail.com>
2020-11-15 21:26:40 -0500

    Revert and cleanup expandlist, startgoal xpath appended begin/end

--- graph_getExpandList.m
+++ graph_getExpandList.m
@@ -3,12 +3,8 @@
 %(line  it:expansion in Algorithm  alg:astar).
 function [idxExpand]=graph_getExpandList(graphVector,idxNBest,idxClosed)
     idxExpand = graphVector(idxNBest).neighbors;
-%     [~,pos] = intersect(idxExpand,idxClosed);
-%     idxExpand(pos) = [];
-%     idxExpand(idxExpand==idxNBest) = [];
-
     idxExpand = setdiff(idxExpand,idxClosed);
-    idxExpand = setdiff(idxExpand,idxNBest);
+    idxExpand(idxExpand==idxNBest) = [];
 end
 %Ensure that the vector  @x   idxBest is not included in the list of neighbors
 %(i.e., avoid self-loop edges in the graph).
